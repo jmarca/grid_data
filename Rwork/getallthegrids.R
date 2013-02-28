831a068d2912fac0e1c0787a51c60471d77ae533
James E. Marca <jmarca@translab.its.uci.edu>
2013-02-27 17:49:20 -0800

    working through saving predicitons to couchdb
    seems to work, running for one month, SJV to test out
    
    modified:   Rwork/fetchFiles.R
    modified:   Rwork/getallthegrids.R
    modified:   Rwork/loadJSON.R
    
    The code is not ready for running as a script, and will need to be
    refactored but it worked for 20 sites so far run manually.

--- Rwork/getallthegrids.R
+++ Rwork/getallthegrids.R
@@ -113,34 +113,54 @@ runme <- function(){
       ## df.data <- get.raft.of.grids(df.grid,month=month,year=year,local=FALSE)
       ## data.pred will model the data, and then predict median fraction
       ## for passed in hpms grids
-      post.gp.fit <- data.model(df.data)
-      
-      # filename <- paste('gridfit','sjv',year,month,i,'RData',sep='.')
-      # save(post.gp.fit,df.data,file=filename,compress='xz')
 
-      # loop and simulate
-      simlim <- length(hpms.subset[,1])
-      picker <- 1:simlim
-      # random order
-      picker <- sample(picker)
-      # just do one at a time for now
-      df.pred.result = data.frame()
-      for(iter in 1:20){ ##simlim
-        sim.set <- picker[iter]
-        df.pred.grid <- hpms.subset[sim.set,]
+
+      df.all.predictions <- data.frame()
+      for(variable in c('n.aadt.frac','hh.aadt.frac','nhh.aadt.frac')){
+        post.gp.fit <- data.model(df.data,formula=formula(paste(variable,1,sep='~')))
         
+        ## loop and simulate
+        simlim <- length(hpms.subset[,1])
+        picker <- 1:simlim
+                                        # just do one at a time for now
+        df.pred.result = data.frame()
         ts.un <- sort(unique(df.data$ts2))
         n.times = length(ts.un)
-        
-        grid.pred <-  data.predict(post.gp.fit,df.pred.grid,ts.un)
-        if(dim(df.pred.result)[1]==0){
-          df.pred.result <<- data.frame(grid.pred$Median)
-        }else{
-          df.pred.result <<- cbind(df.pred.result,grid.pred$Median)
+        for(iter in 1:simlim){ 
+          sim.set <- picker[iter]
+          df.pred.grid <- hpms.subset[sim.set,]
+          
+          grid.pred <-  data.predict(post.gp.fit,df.pred.grid,ts.un)
+          ## save this in df.pred.result
+          
+          df.predicted <- data.frame(var=grid.pred$Median)
+          names(df.predicted) <- variable
+          df.predicted$i_cell <- hpms.subset[sim.set,'i_cell']
+          df.predicted$j_cell <- hpms.subset[sim.set,'j_cell']
+          df.predicted$geom_id<- hpms.subset[sim.set,'geo_id']
+          if(dim(df.pred.result)[1]==0){
+            df.pred.result <<- df.predicted
+          }else{
+            df.pred.result <<- rbind(df.pred.result,df.predicted)
+          }
         }
-          names(df.pred.result[iter]) <-sim.set
+        df.pred.result$tsct <- sort(unique( df.data$tsct))
+        ## now save the predictions in df.prediciton, and loop over the variables
+        if(dim(df.all.predictions)[1]==0){
+          df.all.predictions <<- df.pred.result
+        }else{
+          ## use merge here
+          df.all.predictions <<- merge(df.all.predictions,df.pred.result)
+        }
       }
       
+      ## now dump that back into couchdb
+      ## slap on ts from the original data
+      df.all.predictions$ts = sort(unique(df.data$ts))
+      df.all.predictions$tsct <- NULL
+      rnm = names(df.all.predictions)
+      names(df.all.predicictions) <- gsub('.aadt.frac','',x=rnm)
+      couch.bulk.docs.save('carb%2Fgrid%2Fstate4k',df.all.predictions,local=TRUE,makeJSON=dumpPredictionsToJSON)
     }
   }
 }

6649c6f311a746711c11489473f33ea341b34fa2
James E. Marca <jmarca@translab.its.uci.edu>
2013-10-21 09:53:06 -0700

    more unique differences in vds version vs hpms docs
    swith nhh to not_hh in emit statements
    
    modified:   couchdb/views/grid_to_area/lib/dump_airbasin_membership_vds.js
    modified:   couchdb/views/grid_to_area/lib/dump_airdistrict_membership_vds.js
    modified:   couchdb/views/grid_to_area/lib/dump_county_membership_vds.js

--- couchdb/views/grid_to_area/lib/dump_county_membership_vds.js
+++ couchdb/views/grid_to_area/lib/dump_county_membership_vds.js
@@ -3,7 +3,7 @@ module.exports = function(doc){
         var lookup = require('views/lib/cellmembership').lookup
         var ts_match = /(\d*)-0?(\d*)-0?(\d*)/.exec(doc.data[0])
 
-        emit([lookup[doc.geom_id].county,+ts_match[1],doc.geom_id,+ts_match[2],+ts_match[3]],[doc.aadt_frac.n,doc.aadt_frac.hh,doc.aadt_frac.nhh])
+        emit([lookup[doc.geom_id].county,+ts_match[1],doc.geom_id,+ts_match[2],+ts_match[3]],[doc.aadt_frac.n,doc.aadt_frac.hh,doc.aadt_frac.not_hh])
     }
     return null
 }

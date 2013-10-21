88ca143a99e845ac86b2dcafda997042d79a8b7c
James E. Marca <jmarca@translab.its.uci.edu>
2013-10-21 09:45:56 -0700

    oops, forgot to rename the files in the prior commit
    the *_vds versions are for the database carb%2Fgrid%2Fstate4k
    
    new file:   couchdb/views/grid_to_area/lib/dump_airbasin_membership_vds.js
    new file:   couchdb/views/grid_to_area/lib/dump_airdistrict_membership_vds.js
    new file:   couchdb/views/grid_to_area/lib/dump_county_membership_vds.js
    
    reverted the mistaken edits on these
    modified:   couchdb/views/grid_to_area/lib/dump_airdistrict_membership.js
    modified:   couchdb/views/grid_to_area/lib/dump_county_membership.js

---
+++ couchdb/views/grid_to_area/lib/dump_airbasin_membership_vds.js
@@ -0,0 +1,9 @@
+module.exports = function(doc){
+    if(doc.geom_id !== undefined && doc.aadt_frac !== undefined){
+        var lookup = require('views/lib/cellmembership').lookup
+        var ts_match = /(\d*)-0?(\d*)-0?(\d*)/.exec(doc.data[0])
+
+        emit([lookup[doc.geom_id].airbasin,+ts_match[1],doc.geom_id,+ts_match[2],+ts_match[3]],[doc.aadt_frac.n,doc.aadt_frac.hh,doc.aadt_frac.nhh])
+    }
+    return null
+}

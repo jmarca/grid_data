0a9921cfb0cc4b1ce167e0743a20a15bd8af880c
James E. Marca <james@activimetrics.com>
2015-12-13 10:21:04 -0800

    use rcouchutils to save, post docs

--- R/data.model.R
+++ R/data.model.R
@@ -105,7 +105,7 @@ data.model.and.predict <- function(df.data,df.hpms.grids,year,local=TRUE){
 
     picker <- 1:length(df.hpms.grids[,1])
     couch.test.docs <- paste(df.hpms.grids$geo_id,couch.test.date,sep='_')
-    result = couch.allDocsPost(hpms.grid.couch.db,couch.test.docs,include.docs=FALSE,local=local)
+    result = rcouchutils::couch.allDocsPost(hpms.grid.couch.db,couch.test.docs,include.docs=FALSE,local=local)
     rows = result$rows
     print(length(rows))
     hpmstodo <- picker < 0 # default false
@@ -142,7 +142,7 @@ data.model.and.predict <- function(df.data,df.hpms.grids,year,local=TRUE){
                 rnm = names(df.all.predictions)
                 names(df.all.predictions) <- gsub('.aadt.frac','',x=rnm)
                 save.these = ! is.na(df.all.predictions$n)
-                couch.bulk.docs.save(hpms.grid.couch.db,df.all.predictions[save.these,],local=TRUE,makeJSON=dumpPredictionsToJSON)
+                rcouchutils::couch.bulk.docs.save(hpms.grid.couch.db,df.all.predictions[save.these,],local=TRUE,makeJSON=dumpPredictionsToJSON)
             }
         }
     }else{

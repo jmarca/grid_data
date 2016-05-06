b8a5de6d5003cb6b1a03bb6c341581a0b6f31de2
James E. Marca <james@activimetrics.com>
2016-05-06 10:55:56 -0700

    set up a preloading function
    Rather than going through the data modeling and loading from sources, it
    is a little more efficient to load up lots of work on couchdb/postgresql
    in one pass, then go through later with the modeling pass. So downtime
    "modeling" is skipped.

---
+++ R/data.prep.only.R
@@ -0,0 +1,95 @@
+##' Just preload data from couchdb, etc
+##'
+##' This function is similar to processing.sequence, but it does not
+##' model, and does not predict.  It only loads the data into the fs
+##' for use by the modeling runs.
+##'
+##' @title preloading.sequence
+##' @param df.fwy.grid the freeway grid cells
+##' @param df.hpms.grid.locations the hpms grid cells
+##' @param year the year of this analysis
+##' @param month the month
+##' @param day the day
+##' @param basin the basin (make this unique with iterant too)
+##' @return number left to do
+##' @author James E. Marca
+preloading.sequence <- function(df.fwy.grid,
+                                df.hpms.grid.locations,
+                                year,month,day,basin){
+
+    iter <- 0
+    curlH <- RCurl::getCurlHandle()
+
+    var.models <- list() # fetch.model(year,month,day,basin)
+    df.fwy.data <- fetch.fwy.data(year,month,day,basin)
+    hpms <- fetch.hpms(year,month,day,basin)
+
+    if(length(dim(hpms)) == 2 && length(hpms[,1])<1){
+        print(dim(hpms))
+
+        print(paste('not going to start, all done'))
+        ## rm(df.fwy.data)
+
+        return (0)
+    }
+
+    if(length(df.fwy.data) == 0){
+
+        df.fwy.data <- get.raft.of.grids(df.fwy.grid
+                                        ,day=day
+                                        ,month=month
+                                        ,year=year)
+        stash.fwy.data(year,month,day,basin,df.fwy.data)
+    }
+    if(length(dim(hpms)) == 0){
+
+        hpms <- no.overlap(df.fwy.data,df.hpms.grid.locations)
+
+        hpms <- necessary.grids(df.fwy.data,hpms,year,curlH)
+        stash.hpms(year,month,day,basin,hpms)
+
+
+    }
+
+    if(length(hpms[,1])<1){
+        print(paste('all done'))
+        ## rm(df.fwy.data)
+
+        ## save empties to reduce space needs
+        stash.fwy.data(year,month,day,basin,list())
+        return (0)
+    }else{
+        print(length(hpms[,1]))
+    }
+    rm(curlH)
+    return(length(hpms[,1]))
+}
+
+##' Preload a month of data day by day
+##'
+##' First step is to get the hour by hour data for the freeway grid
+##' cells.  The second step is to model and predict, using plyr, one
+##' day at a time, by running the preloading.sequence function
+##'
+##' This can get out of hand.  RAM bug starts here
+##'
+##' @title preload.data.by.day
+##' @param df.grid the freeway grid cells, but not the data
+##' @param df.hpms.grids the hpms grid cells
+##' @param year the year of analysis
+##' @param month the month to run this.
+##' @param day the day
+##' @param basin the basin (make it unique)
+##' @return nothing at all
+##' @author James E. Marca
+preload.data.by.day <- function(df.grid,df.hpms.grids,year,month,day,basin,maxiter=2){
+    print (paste(year,month,day,pryr::mem_used()))
+    ## don't care about true number of days per month
+    returnval <- preloading.sequence(df.grid,df.hpms.grids
+                                    ,year=year
+                                    ,month=month
+                                    ,day=day
+                                    ,basin=basin)
+
+    return (returnval)
+}
---
+++ R/preload_allthegrids.R
@@ -0,0 +1,105 @@
+##' The main function that does everything.
+##'
+##' @title runme
+##' @return null
+##' @author James E. Marca
+##' @export
+run.preloading <- function(){
+
+    year = as.numeric(Sys.getenv(c("CARB_GRID_YEAR"))[1])
+    month = as.numeric(Sys.getenv(c("CARB_GRID_MONTH"))[1])
+    day = as.numeric(Sys.getenv(c("CARB_GRID_DAY"))[1])
+    basin = Sys.getenv(c("AIRBASIN"))[1]
+
+    cl <- NULL
+    df.hpms.grids <- NULL
+    df.grid.data <- NULL
+
+    retrieve.result <- fetch.outer.data(year,month,day,basin)
+
+
+    if(length(retrieve.result) == 3){
+        cl <- retrieve.result$cl
+        df.hpms.grids <- retrieve.result$df.hpms.grids
+        df.grid.data <- retrieve.result$df.grid.data
+    }
+    if(length(dim(df.hpms.grids))>0){
+        # okay data saved
+    }else{
+        ## load data from couchdb attachments, if available
+        df.grid <- load.grids.with.hwy(basin,year)
+
+        ## cluster **ONLY** the grid cells with valid data
+        data.count <- get.rowcount.of.grids(df.grid,
+                                            day=day,month=month,year=year)
+
+        df.grid.data <- df.grid[data.count>0,]
+
+
+
+        if(nrow(df.grid.data) == 0){
+            print('skipping, no data')
+            return (0)
+        }
+        print('dim df hwy grid')
+        print(dim(df.grid.data))
+
+
+        df.hpms.grids <- load.grids.with.hpms(basin,year)
+        print('dim df hpms grid')
+        print(dim(df.hpms.grids))
+
+
+        ## remember, fake days have no data, so you're safe here
+
+        ## want clusters of about 20
+        numclust <- ceiling(dim(df.grid.data)[1] / 20)
+        if(numclust > 10) numclust = 10
+        print(paste('numclust is ',numclust,'num grid cells is',nrow(df.grid.data)))
+        cl <- NULL
+        if(numclust > 1){
+            cl <- cluster::clara(as.matrix(df.grid.data[,c('lon','lat')]),numclust,pamLike = TRUE,samples=100)
+            centers <- as.data.frame(cl$medoids)
+            centers$clustering = cl$clustering[rownames(cl$medoids)]
+
+            ## create the assigning function based on the clustered centers
+            ascl <- assign.hpms.grid.cell(centers)
+            df.hpms.grids$cluster <- -1
+            for(i in 1:length(df.hpms.grids$lat)){
+                df.hpms.grids$cluster[i] <- ascl(df.hpms.grids[i,])
+            }
+        }else{
+            ## everything is in one cluster
+            df.hpms.grids$cluster <- 1
+            cl <- data.frame('clustering'=1)
+        }
+
+        ## stash all of: cl, df.hpms.grids, df.grid.data by year, month,
+        ## day, basin
+
+        stash(year,month,day,basin,cl,df.hpms.grids,df.grid.data)
+    }
+
+    print(paste('preloading',basin,year,month,day))
+
+    ## first make sure that the clusters are not too big.  if so, catch next pas
+    numclust <- max(df.hpms.grids$cluster)
+
+    returnval <- 0
+
+    for(cl.i in 1:numclust){
+        print(paste('cluster',cl.i,'of',numclust))
+        grid.idx <- cl$clustering==cl.i
+        hpms.idx <- df.hpms.grids$cluster==cl.i
+        somereturnval <- preload.data.by.day(df.grid.data[grid.idx,]
+                                            ,df.hpms.grids[hpms.idx,]
+                                            ,year=year
+                                            ,month=month
+                                            ,day=day
+                                            ,basin=paste(basin,cl.i,numclust
+                                                        ,sep='_')
+                                             )
+        returnval <- max(returnval,somereturnval )
+    }
+    return (returnval)
+}
